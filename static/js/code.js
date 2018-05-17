fc6adf0d32d65cff228d95e692a34df7e8e54e58
MV Admin <mvadmin@MVCC-MACMINI-86.local>
2018-05-16 19:33:58 -0700

    finished

--- static/js/code.js
+++ static/js/code.js
@@ -1,21 +1,19 @@
 var canvas = document.getElementById("board");
 var context = canvas.getContext("2d");
-var size = 50;
+var size = 100;
 var sizeOfSquares = 25;
 var ant = {
   x: size/2,
   y: size/2,
   dir: 0
 };
+var steps = 0;
 
 var board = [];
 
 function startUp() {
-  // for (var i = 0; i < board.length; i++) {
-  //   for (var n = 0; n < board.length; n++) {
-  //     board[i][n] = false;
-  //   }
-  // }
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
 
   for (var j = 0; j < size; j++) {
     var tempArray = [];
@@ -24,35 +22,50 @@ function startUp() {
     }
     board.push(tempArray);
   }
+
+  context.fillStyle = "blue";
+
+  for (var i = 0; i < board.length; i++) {
+    for (var n = 0; n < board[i].length; n++) {
+      context.fillRect(n * (sizeOfSquares + 5), i * (sizeOfSquares + 5), sizeOfSquares, sizeOfSquares);
+    }
+  }
+
   draw();
 }
 
 function draw() {
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+  // Change Direction
 
-  for (var i = 0; i < board.length; i++) {
-    for (var n = 0; n < board.length; n++) {
-      if (board[i][n]) {
-        context.fillStyle = "red";
-      } else {
-        context.fillStyle = "blue";
-      }
-      context.fillRect(i * (sizeOfSquares + 5), n * (sizeOfSquares + 5), sizeOfSquares, sizeOfSquares);
-    }
-  }
-  console.log(board);
-  console.log(ant);
-
-  if (!board[ant.y][ant.x]) {
-    ant.dir += 90;
-  } else {
+  if (board[ant.y][ant.x]) {
     ant.dir -= 90;
+    context.fillStyle = "blue"; // Have this here so that I don't have to make a seperate if statement and slow down the computer
+  } else {
+    ant.dir += 90;
+    context.fillStyle = "red"; // They are opposites the usual because it changes the state later
   }
+
+  // Make direction in between 0 and 360
 
   ant.dir %= 360;
-  ant.dir = Math.abs(ant.dir);
+  if (ant.dir < 0) {
+    ant.dir += 360;
+  }
+
+  // Change state of square ant is on
+
   board[ant.y][ant.x] = !board[ant.y][ant.x];
+
+  // Draw the change in state
+
+  // if (board[ant.y][ant.x]) {
+  //   context.fillStyle = "red";
+  // } else {
+  //   context.fillStyle = "blue";
+  // }
+  context.fillRect(ant.x * (sizeOfSquares + 5), ant.y * (sizeOfSquares + 5), sizeOfSquares, sizeOfSquares);
+
+  // Move the ant according to direction
 
   if (ant.dir == 0) {
     ant.y--;
@@ -64,10 +77,15 @@ function draw() {
     ant.x--;
   }
 
-  console.log(board);
-  console.log(ant);
+  // Draw the change in ant's position
 
-  if (ant.x >= 0 && ant.y >= 0 && ant.x <= board.length && ant.y <= board.length) {
+  context.fillStyle = "yellow";
+  context.fillRect(ant.x * (sizeOfSquares + 5), ant.y * (sizeOfSquares + 5), sizeOfSquares, sizeOfSquares);
+
+  // Make the ant move again if the ant didn't reach the edge
+
+  if (ant.x >= 0 && ant.y >= 0 && ant.x < board.length && ant.y < board.length) {
+    steps++;
     setTimeout(draw, 1);
   }
 }
